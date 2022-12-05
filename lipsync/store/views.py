b4c2b9317a6d627a318547c3832041c03801077f
oyekamal <autocratickamal1@gmail.com>
2022-12-05 14:33:54 +0500

    chnages

--- lipsync/store/views.py
+++ lipsync/store/views.py
@@ -8,6 +8,7 @@ def home(request):
 
     return render(request, 'store/home.html')
 
+
 def test(request):
     print(" testing...")
     if request.method == 'GET':
@@ -16,8 +17,8 @@ def test(request):
         mydict = {
             'form': form,
         }
-        return render(request, 'store/test.html',context=mydict)
-    elif request.method=="POST":
+        return render(request, 'store/test.html', context=mydict)
+    elif request.method == "POST":
         print(" -------------- post --------------------------------    ")
         form = UserForm(request.POST)
         if form.is_valid():
@@ -25,35 +26,29 @@ def test(request):
         return redirect('/')
     else:
         return redirect('/')
-       
 
 
 def Fileuploadrederer(request):
     print(request)
     if request.method == 'GET':
-        print("get file uploadrederer is called with")
         form = FileUploadForm
         mydict = {
             'form': form,
         }
-        return render(request, 'store/upload.html',context=mydict)
-        
+        return render(request, 'store/upload.html', context=mydict)
+
     else:
-        print("post is getting called   ")
         try:
             form = FileUploadForm(request.POST, request.FILES)
             if form.is_valid():
-            
+
                 form.save()
+                print(form)
             else:
                 print(form.errors())
             # {'csrfmiddlewaretoken': ['gue4rN9myMcCPO09Y0PrHK54FyUd2V0Fp8OAM6eRzJJw3bww2femxtKPF10WlGoU'], 'audio': ['software4.wav'], 'script': ['software4.txt'], 'remark': ['asd']}>
             # File.objects.create(audio=request.POST['audio'],script=request.POST['script'],remark=request.POST['remark'])
         except Exception as e:
             print(e)
-        # form = FileUploadForm(request.POST)
-        # breakpoint()
-        # if form.is_valid():
-        #     form.save()
+
         return redirect('/')
-       
