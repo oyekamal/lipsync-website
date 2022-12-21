87d3e2e9158f31616565719a4944ce82c0bf9a06
oyekamal <autocratickamal1@gmail.com>
2022-12-21 16:14:48 +0500

    static settings

--- app/core/local_settings.py
+++ app/core/local_settings.py
@@ -170,14 +170,20 @@ Q_CLUSTER = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
 
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
+
+# # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
