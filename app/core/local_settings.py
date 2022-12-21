f68f5dab5a493a69881bdfaea4e10d7c2aa88505
oyekamal <autocratickamal1@gmail.com>
2022-12-21 16:55:10 +0500

    last staticfiles change

--- app/core/local_settings.py
+++ app/core/local_settings.py
@@ -172,13 +172,13 @@ Q_CLUSTER = {
 
 # settings.py
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
